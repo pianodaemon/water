79157c5346ccb21d14dc8db9213e6e1bca5cfcf1
Edwin Plauchu <pianodaemon@gmail.com>
2023-10-18 11:06:07 -0600

    Create ctrl.py

---
+++ ctrl.py
@@ -0,0 +1,16 @@
+import logging
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "ps")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "kb")))
+
+class CtrlError(Exception):
+    def __init__(self, message = None):
+        self.message = message
+    def __str__(self):
+        return self.message
+
+class CtrlModuleError(CtrlError):
+    def __init__(self, message = None, module = None):
+        super().__init__(message = message)
