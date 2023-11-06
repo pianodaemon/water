a8e16895901fc8bfb5b9a9defe7c6224d79584e9
Edwin Plauchu <j4nusx@yahoo.com>
2023-11-05 21:52:38 -0600

    test_stop_cycle

--- robot/fgc.py
+++ robot/fgc.py
@@ -1,17 +1,22 @@
 import time
+import queue
+import logging
+import multiprocessing as mp
+
 
 class FlowGateCycle(object):
     '''Perform the flow gate cycle'''
 
-    __SENTINEL_INTERVAL_MS = 0.05
+    __SENTINEL_INTERVAL = 0
 
-    def __init__(self, interval, open_handler, close_handler):
-        if interval <= self.__SENTINEL_INTERVAL_MS:
-            emsg = "Interval can not elapse less than {}".format(self.__SENTINEL_INTERVAL_MS)
+    def __init__(self, stop_queue, interval, open_handler, close_handler):
+        self._logger = logging.getLogger()
+        if interval <= self.__SENTINEL_INTERVAL:
+            emsg = "Interval can not be zero or elapse less than {}".format(self.__SENTINEL_INTERVAL)
             raise Exception(emsg)
+        self._qstop = stop_queue
         self._interval = interval
         self._open_handler, self._close_handler = open_handler, close_handler
-        self._getoff = False
 
     def __enter__(self):
         self._open_handler()
@@ -20,13 +25,31 @@ class FlowGateCycle(object):
         self._hold_on()
         self._close_handler()
 
-    def _time_gap(self):
-        t = time.time()
-        return t, t + self._interval
-
     def _hold_on(self):
-        tbegin, tend = self._time_gap()
-        while not self._getoff:
-            time.sleep(self.__SENTINEL_INTERVAL_MS)
-            if time.time() >= tend:
-                self._getoff = True
+        try:
+            msg = self._qstop.get(True, self._interval)
+            if msg is None: # We send this as a sentinel to stop
+                self._logger.info("It was summoned a stop")
+            else:
+                raise Exception("We can not summon a stop by passing any value")
+        except queue.Empty:
+            self._logger.info("Lapse to summon a stop is over")
+        except:
+            raise Exception("Perhaps the queue is closed")
+
+    @classmethod
+    def _cycle_process(cls, queue, interval, og_callback, cg_callback, delta):
+        start_time = 0
+        with cls(queue, interval, og_callback, cg_callback) as flow:
+            start_time = time.time()
+        delta.value = time.time() - start_time
+
+    @classmethod
+    def sluice(cls, stop_queue, interval, og_callback, cg_callback):
+        delta = mp.Value('f', 0.0)
+        proc = mp.Process(
+                target=cls._cycle_process,
+                args=(stop_queue, interval, og_callback, cg_callback, delta))
+        proc.start()
+        proc.join()
+        return delta.value
--- robot/test_fgc.py
+++ robot/test_fgc.py
@@ -1,22 +1,43 @@
 from unittest import TestCase
 from .fgc import FlowGateCycle
 import time
+import multiprocessing as mp
+
 
 class TestFlowGateCycle(TestCase):
 
+    @staticmethod
+    def _stop_process(q, delay):
+        time.sleep(delay)
+        q.put(None)
+
+    def test_stop_cycle(self):
+        '''The cycle is never interrupted'''
+        stop_queue = mp.Queue(1)
+        def og_callback():
+            pass
+        def cg_callback():
+            pass
+
+        # It will trigger stop in 3 seconds away
+        proc = mp.Process(target=self._stop_process, args=(stop_queue, 3))
+        proc.start()
+
+        # During six seconds the liquid shall flow
+        interval = 6
+        gap = FlowGateCycle.sluice(stop_queue, interval, og_callback, cg_callback)
+        # More than 2 seconds should have been left as time non-used
+        self.assertTrue((interval - int(gap)) >= 2)
+
     def test_happy_path(self):
         '''The cycle is never interrupted'''
-        start_time, stop_time = 0, 0
-        def open_handler():
+        stop_queue = mp.Queue(1)
+        def og_callback():
             pass
-        def close_handler():
+        def cg_callback():
             pass
 
         # During three seconds the liquid shall flow
         interval = 3
-        with FlowGateCycle(interval, open_handler, close_handler) as rflow:
-            start_time = time.time()
-        stop_time = time.time()
-        # It shall truncate decimal part
-        gap = int(interval - (stop_time - start_time))
-        self.assertTrue(gap == 0)
+        gap = FlowGateCycle.sluice(stop_queue, interval, og_callback, cg_callback)
+        self.assertTrue((interval - int(gap)) == 0)
